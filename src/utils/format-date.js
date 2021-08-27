c1d8b22bbab026403cc53237f6d12dd897f9c36c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-08-27 13:12:13 +0200

    formatDateTime function using luxon

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,20 +1,18 @@
+import { DateTime } from 'luxon';
+
 export function formatDate(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  return new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   }).format(new Date(timestamp));
 }
 
-export function formatDateTime(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZoneName: "short",
-  }).format(new Date(timestamp));
+export function formatDateTime(timestamp, keepTimeZone = false) {
+  return new DateTime.fromISO(timestamp, {
+    setZone: keepTimeZone,
+  })
+    .setLocale('en')
+    .toLocaleString({ ...DateTime.DATETIME_FULL_WITH_SECONDS });
 }
