79274c3a9b1accd3318822f8c443314539075894
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-09 23:19:34 +0200

    create function to convert filter context vars to query params

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
 export const isFavorite = (favorites, itemId) => {
   return favorites.find(({ id }) => id === itemId);
 };
+
+export const filters2params = (filters) => {
+  let params = {};
+  Object.keys(filters).forEach((key) => {
+    if (key === 'launch_success' && filters[key].length === 1) {
+      const value = filters[key][0] === 'successful' ? 'true' : 'false';
+      params = { ...params, launch_success: value };
+    }
+  });
+  return params;
+};
