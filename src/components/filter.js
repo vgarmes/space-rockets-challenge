6472a2689472931c5c8fc60a5fe34b66a753a139
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-09 23:18:58 +0200

    create filter component

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  Box,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionIcon,
+  AccordionPanel,
+  CheckboxGroup,
+  Checkbox,
+  Text,
+} from '@chakra-ui/core';
+import { useLaunchesContext } from '../context/launches_context';
 
 const Filter = () => {
-  return <div></div>;
+  return (
+    <Accordion defaultIndex={[0]} allowToggle mb={2}>
+      <AccordionItem border="0">
+        <AccordionHeader px="0">
+          <Box
+            flex="1"
+            textAlign="left"
+            fontSize="sm"
+            fontWeight="bold"
+            textTransform="uppercase"
+            letterSpacing="wider"
+          >
+            Filter
+          </Box>
+          <AccordionIcon />
+        </AccordionHeader>
+        <AccordionPanel pl={1} pb={4}>
+          <SuccessCheckbox />
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const SuccessCheckbox = () => {
+  const { filters, updateFilters } = useLaunchesContext();
+  const handleChange = (newValues) => {
+    let tempValues = [...newValues];
+    if (newValues.length === 0) {
+      tempValues =
+        filters.launch_success[0] === 'successful'
+          ? ['failed']
+          : ['successful'];
+    }
+    updateFilters('launch_success', tempValues);
+  };
+  return (
+    <CheckboxGroup
+      isInline
+      spacing={4}
+      size="sm"
+      name="launch_success"
+      value={filters.launch_success}
+      onChange={handleChange}
+    >
+      <Checkbox value="successful">Successful</Checkbox>
+      <Checkbox value="failed">Failed</Checkbox>
+    </CheckboxGroup>
+  );
 };
 
 export default Filter;
