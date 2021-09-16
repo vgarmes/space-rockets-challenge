597eda0798b72c10413023a1880501066d5d886a
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-16 15:05:19 +0200

    pass isHome prop to navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Box } from '@chakra-ui/core';
 import Navbar from './navbar';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+
   return (
     <>
-      <Navbar />
+      <Navbar isHome={pathname === '/'} />
       <Box mx={[2, null, 6]}>{children}</Box>
     </>
   );
