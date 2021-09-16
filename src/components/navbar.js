5a0941228cdb051e3bce34ab45a38698eea5541d
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-16 15:05:49 +0200

    animated logo and menu button on initial render

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,35 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Flex, Text, Button, useDisclosure } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import { animationTimeout } from '../constants';
+import usePrefersReducedMotion from '../utils/usePrefersReducedMotion';
 import DrawerMenu from './menu';
 
-const Navbar = () => {
+const Navbar = ({ isHome }) => {
+  const [isMounted, setIsMounted] = useState(!isHome);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const drawerBtnRef = useRef();
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  const timeout = isHome ? animationTimeout : 0;
+  const fadeDownClass = isHome ? 'fadedown' : '';
+  const nodeRef = useRef(null);
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const mountTimeout = setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
+
+    return () => {
+      clearTimeout(mountTimeout);
+    };
+  }, [prefersReducedMotion]);
+
   return (
     <Flex
       as="nav"
@@ -15,20 +39,65 @@ const Navbar = () => {
       padding="6"
       bg="gray.800"
       color="white"
+      minHeight="88px"
     >
-      <Text
-        as={Link}
-        to="/"
-        fontFamily="mono"
-        letterSpacing="2px"
-        fontWeight="bold"
-        fontSize="lg"
-      >
-        ¡SPACE·R0CKETS!
-      </Text>
-      <Button ref={drawerBtnRef} variant="outline" onClick={onOpen}>
-        Menu
-      </Button>
+      {prefersReducedMotion ? (
+        <>
+          <Text
+            as={Link}
+            to="/"
+            fontFamily="mono"
+            letterSpacing="2px"
+            fontWeight="bold"
+            fontSize="lg"
+          >
+            ¡SPACE·R0CKETS!
+          </Text>
+
+          <Button ref={drawerBtnRef} variant="outline" onClick={onOpen}>
+            Menu
+          </Button>
+        </>
+      ) : (
+        <>
+          <CSSTransition
+            in={isMounted}
+            nodeRef={nodeRef}
+            classNames={fadeDownClass}
+            timeout={timeout}
+            unmountOnExit
+          >
+            <Text
+              ref={nodeRef}
+              as={Link}
+              to="/"
+              fontFamily="mono"
+              letterSpacing="2px"
+              fontWeight="bold"
+              fontSize="lg"
+            >
+              ¡SPACE·R0CKETS!
+            </Text>
+          </CSSTransition>
+          <CSSTransition
+            in={isMounted}
+            nodeRef={drawerBtnRef}
+            classNames={fadeDownClass}
+            timeout={timeout}
+            unmountOnExit
+          >
+            <Button
+              ref={drawerBtnRef}
+              variant="outline"
+              onClick={onOpen}
+              style={{ transitionDelay: `${isHome ? 200 : 0}ms` }}
+            >
+              Menu
+            </Button>
+          </CSSTransition>
+        </>
+      )}
+
       <DrawerMenu isOpen={isOpen} onClose={onClose} btnRef={drawerBtnRef} />
     </Flex>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const animationTimeout = 2000; // timeout between animation classes -active and -done (not -enter and -active)
