e137245f962a495ccdb0e33e43589af258323892
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-16 16:31:54 +0200

    added fade animations for buttons

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,46 @@
-import React from "react";
-import { Flex, Box, Text, Stack, Link } from "@chakra-ui/core";
-import { ArrowRight } from "react-feather";
-import { Link as BrowserLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Flex, Box, Text, Stack, Link } from '@chakra-ui/core';
+import { ArrowRight } from 'react-feather';
+import { Link as BrowserLink } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import usePrefersReducedMotion from '../utils/usePrefersReducedMotion';
+import { animationTimeout } from '../constants';
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const mountTimeout = setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
+
+    return () => {
+      clearTimeout(mountTimeout);
+    };
+  }, [prefersReducedMotion]);
+
   return (
     <Stack m="6" spacing="6">
-      <PageLink url="/launches">Browse SpaceX Launches</PageLink>
-      <PageLink url="/launch-pads">Browse SpaceX Launch Pads</PageLink>
+      <CustomCSSTransition
+        classNames="fade"
+        timeout={animationTimeout}
+        trigger={isMounted}
+      >
+        <PageLink url="/launches">Browse SpaceX Launches</PageLink>
+      </CustomCSSTransition>
+      <CustomCSSTransition
+        classNames="fade"
+        timeout={animationTimeout}
+        delay={250}
+        trigger={isMounted}
+      >
+        <PageLink url="/launch-pads">Browse SpaceX Launch Pads</PageLink>
+      </CustomCSSTransition>
     </Stack>
   );
 }
@@ -28,3 +61,24 @@ function PageLink({ url, children, ...rest }) {
     </Link>
   );
 }
+
+const CustomCSSTransition = ({
+  children,
+  trigger,
+  classNames,
+  timeout,
+  delay,
+  ...rest
+}) => {
+  return (
+    <CSSTransition
+      in={trigger}
+      classNames={classNames}
+      timeout={timeout}
+      style={{ transitionDelay: `${delay ? delay : '0'}ms` }}
+      unmountOnExit
+    >
+      {React.cloneElement(children, { ...rest })}
+    </CSSTransition>
+  );
+};
