4773905a130f75ad523e4f3e5d8b99e0b0ab2516
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-20 09:23:18 +0200

    added display options

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
-export const animationTimeout = 2000; // timeout between animation classes -active and -done (not -enter and -active)
+export const MIN_CHILD_WIDTH = 350; // minimum width of each card in the grid view of launches
+export const PAGE_SIZE = 12; // limit of elements to retrieve from each api call
