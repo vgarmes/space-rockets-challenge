e0fa9ff0f34ae884341810cc3b210766f3f9c088
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-20 09:17:50 +0200

    removed animations

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,34 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Flex, Text, Button, useDisclosure } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import { animationTimeout } from '../constants';
-import usePrefersReducedMotion from '../utils/usePrefersReducedMotion';
 import DrawerMenu from './menu';
 
 const Navbar = ({ isHome }) => {
-  const [isMounted, setIsMounted] = useState(!isHome);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const drawerBtnRef = useRef();
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  const timeout = isHome ? animationTimeout : 0;
-  const fadeDownClass = isHome ? 'fadedown' : '';
-  const nodeRef = useRef(null);
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
-    }
-
-    const mountTimeout = setTimeout(() => {
-      setIsMounted(true);
-    }, 100);
-
-    return () => {
-      clearTimeout(mountTimeout);
-    };
-  }, [prefersReducedMotion]);
 
   return (
     <Flex
@@ -41,57 +18,20 @@ const Navbar = ({ isHome }) => {
       color="white"
       minHeight="88px"
     >
-      {prefersReducedMotion ? (
-        <>
-          <Text
-            as={Link}
-            to="/"
-            fontFamily="mono"
-            letterSpacing="2px"
-            fontWeight="bold"
-            fontSize="lg"
-          >
-            ¡SPACE·R0CKETS!
-          </Text>
+      <Text
+        as={Link}
+        to="/"
+        fontFamily="mono"
+        letterSpacing="2px"
+        fontWeight="bold"
+        fontSize="lg"
+      >
+        ¡SPACE·R0CKETS!
+      </Text>
 
-          <Button ref={drawerBtnRef} variant="outline" onClick={onOpen}>
-            Menu
-          </Button>
-        </>
-      ) : (
-        <>
-          <CSSTransition
-            in={isMounted}
-            nodeRef={nodeRef}
-            classNames={fadeDownClass}
-            timeout={timeout}
-            unmountOnExit
-          >
-            <Text
-              ref={nodeRef}
-              as={Link}
-              to="/"
-              fontFamily="mono"
-              letterSpacing="2px"
-              fontWeight="bold"
-              fontSize="lg"
-            >
-              ¡SPACE·R0CKETS!
-            </Text>
-          </CSSTransition>
-          <CSSTransition
-            in={isMounted}
-            nodeRef={drawerBtnRef}
-            classNames={fadeDownClass}
-            timeout={timeout}
-            unmountOnExit
-          >
-            <Button ref={drawerBtnRef} variant="outline" onClick={onOpen}>
-              Menu
-            </Button>
-          </CSSTransition>
-        </>
-      )}
+      <Button ref={drawerBtnRef} variant="outline" onClick={onOpen}>
+        Menu
+      </Button>
 
       <DrawerMenu isOpen={isOpen} onClose={onClose} btnRef={drawerBtnRef} />
     </Flex>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,46 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Flex, Box, Text, Stack, Link } from '@chakra-ui/core';
 import { ArrowRight } from 'react-feather';
 import { Link as BrowserLink } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import usePrefersReducedMotion from '../utils/usePrefersReducedMotion';
-import { animationTimeout } from '../constants';
 
 export default function Home() {
-  const [isMounted, setIsMounted] = useState(false);
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
-    }
-
-    const mountTimeout = setTimeout(() => {
-      setIsMounted(true);
-    }, 100);
-
-    return () => {
-      clearTimeout(mountTimeout);
-    };
-  }, [prefersReducedMotion]);
-
   return (
     <Stack m="6" spacing="6">
-      <CustomCSSTransition
-        classNames="fade"
-        timeout={animationTimeout}
-        trigger={isMounted}
-      >
-        <PageLink url="/launches">Browse SpaceX Launches</PageLink>
-      </CustomCSSTransition>
-      <CustomCSSTransition
-        classNames="fade"
-        timeout={animationTimeout}
-        trigger={isMounted}
-        delay={250}
-      >
-        <PageLink url="/launch-pads">Browse SpaceX Launch Pads</PageLink>
-      </CustomCSSTransition>
+      <PageLink url="/launches">Browse SpaceX Launches</PageLink>
+      <PageLink url="/launch-pads">Browse SpaceX Launch Pads</PageLink>
     </Stack>
   );
 }
@@ -61,26 +28,3 @@ function PageLink({ url, children, ...rest }) {
     </Link>
   );
 }
-
-const CustomCSSTransition = ({
-  children,
-  trigger,
-  classNames,
-  timeout,
-  delay,
-  ...rest
-}) => {
-  /* props from parent chakra-ui component are passed in {..rest}
-  and default transitions are overriden */
-  return (
-    <CSSTransition
-      in={trigger}
-      classNames={classNames}
-      timeout={timeout}
-      style={{ transitionDelay: `${delay ? delay : '0'}ms` }}
-      unmountOnExit
-    >
-      {React.cloneElement(children, { transition: '', ...rest })}
-    </CSSTransition>
-  );
-};
